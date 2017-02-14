9c317d31ddff5a7e7a4f5e690477a6161d51c688
myan <mike.yan@activenetwork.com>
2017-02-14 10:06:02 +0800

    Add UT .

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -3,5 +3,3 @@ from flask import Blueprint
 main = Blueprint('main', __name__)
 
 from . import views, errors
-
-# main.register_error_handler = errors
--- app/models.py
+++ app/models.py
@@ -2,7 +2,13 @@ from . import db
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), unique = True, index = True)
+    passwd = db.Column(db.String(64))
     def __repr__(self):
         return '<User %r>' % self.username
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key = True)
+    taskname = db.Column(db.String)
