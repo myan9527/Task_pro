dc78ff4c48824f4911ad230b52c8a75ca04b24ee
myan <mike.yan@activenetwork.com>
2017-02-14 15:04:13 +0800

    user module.

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
 from . import db
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique = True, index = True)
     password_hash = db.Column(db.String(128))
 
