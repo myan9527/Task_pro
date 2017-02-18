81e27b558c01140c412821da542da686ef3b8849
Hannah Huo <huochenhui1008@163.com>
2017-02-18 23:08:55 +0800

    Page structure.

--- app/main/views.py
+++ app/main/views.py
@@ -6,4 +6,4 @@ from flask_login import current_user
 def index():
     if current_user.is_authenticated:
         return redirect(url_for('user.dashboard'))
-    return render_template("index.html", title='Index')
+    return render_template("index.html")
