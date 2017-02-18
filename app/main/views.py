5749daaa3ae777273d7b68700acb43747214f19a
Hannah Huo <huochenhui1008@163.com>
2017-02-18 13:17:38 +0800

    small fix

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
 from . import main
-from flask import render_template, request, abort, make_response
+from flask import render_template, url_for, redirect
+from flask_login import current_user
 
 @main.route('/', methods = ['GET'])
 def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('user.dashboard'))
     return render_template("index.html", title='Index')
